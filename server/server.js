3085537130a892909dbf45d6412260cd06154309
kishoreramanavelmurugan@gamil.com <kishoreramanavelmurugan@gmail.com>
2026-07-20 12:15:18 +0530

    atlast done

--- server/server.js
+++ server/server.js
@@ -13,19 +13,23 @@ const allowedOrigins = [
   "http://127.0.0.1:3000"
 ].filter(Boolean);
 
-// Middleware
-app.use(cors({
+const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
       return;
     }
-    callback(new Error("Not allowed by CORS"));
+
+    callback(null, true);
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options(/(.*)/, cors());
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+
+// Middleware
+app.use(cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "client")));
 
